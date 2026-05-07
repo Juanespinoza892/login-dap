3cad580541ba99dc879bf0e4496b297f6d942b12
Juanespinoza892 <49319562@est.ort.edu.ar>
2026-05-07 15:27:59 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import 'Pantallas/estado_1.dart';
+import 'Pantallas/estado_2.dart';
+
+void main() {
+  runApp(const SistemaLogin());
+}
+
+class SistemaLogin extends StatelessWidget {
+  const SistemaLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final GoRouter appRouter = GoRouter(
+      routes: [
+
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const Estado1(),
+        ),
+
+        GoRoute(
+          path: '/inicio',
+          builder: (context, state) => const Estado2(),
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: appRouter,
+      title: "Control de Acceso",
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+    );
+  }
+}
